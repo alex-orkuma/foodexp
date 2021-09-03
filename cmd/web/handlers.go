0523d1a5fdca831a7c96a83b6622ba2a2edf4936
alex-orkuma <orkumashi@gmail.com>
2021-09-03 17:32:00 +0100

    removing bootstrap modules and used cdn also added a dasboard.tmpl.page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,1 +1,53 @@
 package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func dasboard(w http.ResponseWriter, r *http.Request) {
+
+	ts, err := template.ParseFiles("./ui/html/dashboard.page.tmpl")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
+// Add products handler
+func addProduct(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("Adding products to the database...."))
+}
+
+// Get all products handler
+func getProducts(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Getting all products from the database"))
+}
+
+// Get a single product hanler
+func getProduct(w http.ResponseWriter, r *http.Request) {
+
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific product with ID %d...", id)
+}
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,43 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 )
-
-func dasboard(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Welcome to my dashboard..."))
-}
-
-// Add products handler
-func addProduct(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method != http.MethodPost {
-
-		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	w.Write([]byte("Adding products to the database...."))
-}
-
-// Get all products handler
-func getProducts(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Getting all products from the database"))
-}
-
-// Get a single product hanler
-func getProduct(w http.ResponseWriter, r *http.Request) {
-
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-	fmt.Fprintf(w, "Display a specific product with ID %d...", id)
-}
 
 func main() {
 
