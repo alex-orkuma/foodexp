89444f361b926ad151ef2750e7f45e48d2136a4d
alex-orkuma <orkumashi@gmail.com>
2021-09-03 16:54:35 +0100

    re-organized the structure of the code and implemented an additional handler

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func dasboard(w http.ResponseWriter, r *http.Request) {
@@ -11,6 +13,13 @@ func dasboard(w http.ResponseWriter, r *http.Request) {
 
 // Add products handler
 func addProduct(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	w.Write([]byte("Adding products to the database...."))
 }
 
@@ -21,17 +30,23 @@ func getProducts(w http.ResponseWriter, r *http.Request) {
 
 // Get a single product hanler
 func getProduct(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Getting product with id ID..."))
+
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific product with ID %d...", id)
 }
 
 func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/dashboard", dasboard)
+	mux.HandleFunc("/", dasboard)
 	mux.HandleFunc("/product/create", addProduct)
 	mux.HandleFunc("/product", getProducts)
-	mux.HandleFunc("/product/id", getProduct)
+	mux.HandleFunc("/product?id=1", getProduct)
 
 	log.Println("starting a server on port 4000...")
 	err := http.ListenAndServe(":4000", mux)
